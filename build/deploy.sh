110376e7e832ee29783a80e19676668d2363284e
Edgar Lopez <edgar.drummer1993@gmail.com>
2021-03-13 00:30:32 -0600

    Test

--- build/deploy.sh
+++ build/deploy.sh
@@ -9,6 +9,14 @@ LIST_FILES=$(git diff-tree --no-commit-id --name-only -r $COMMIT_SHA)
 FILES=$(echo "$LIST_FILES" | awk -v x="\"" '/^force/ { print x$1x}'  | paste -d","  -s)
 echo "$FILES"
 
+TEST_FILES=$(echo "$LIST_FILES" | awk -v x="\"" '/Test.cls$/ { print $1}'  | paste -d","  -s)
+echo " ++++++++++"
+echo $TEST_FILES
+TEST_FILES=$(echo $TEST_FILES | sed 's/force-app\/main\/default\/classes\///' | sed 's/Test.cls/Test/')
+echo " ++++++++"
+echo $TEST_FILES
+
+
 if [ -z "$FILES" ]
 then
       echo "No files to deploy"
